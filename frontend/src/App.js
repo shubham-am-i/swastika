0fcf33dc0e797c4013169bd00f389b789096c97a
shubham <patilshubham2k21@gmail.com>
2022-06-21 17:43:10 +0530

    implement React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <>
-      <h1>Welcome To Proshop</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className='mt-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>} exact />
+            <Route path='/product/:id' element={<ProductScreen/>} />
+          </Routes>
+        </Container>
+      </main>
+
+      <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ProductScreen = () => {
+  return (
+    <div>
+      Product
+    </div>
+  )
+}
+
+export default ProductScreen
